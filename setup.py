3477bac15948a49032a5b0f297b9c83f3c90d589
Ryan Krauss <ryanwkrauss@gmail.com>
2021-04-11 21:00:04 -0400

    trying to fix rpi stuff

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(
+#with open("README.md", "r") as fh:
+#    long_description = fh.read()
+
+setuptools.setup(
     name='serial_utils',    # This is the name of your PyPI-package.
-    version='0.1',                          # Update the version number for new releases
-    scripts=['serial_utils']                  # The name of your scipt, and also the command you'll be using for calling it
+    version='0.4.1',
+    url='https://github.com/ryanGT/serial_utils',
+    author='Ryan Krauss',
+    author_email='ryanwkrauss@gmail.com',
+    description="package for fascilitating serial communication between python and arduino",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
